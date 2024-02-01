e4a0cbe8d741d75ffd5a8375c7048ccd39cc94cb
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2024-02-01 19:27:36 +0000

    Added typewriter font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ function generateLimerick(event) {
   event.preventDefault();
 
   let poemElement = document.querySelector("#poem");
-  poemElement.innerHTML = "Da Da Dat Dat Da Daaarrr";
+
+  new Typewriter("#poem", {
+    strings: "Da Dat Dat Dat Dat Dat DAAAA",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
 }
 let limerickFormElement = document.querySelector("#limerick-generator-form");
 limerickFormElement.addEventListener("submit", generateLimerick);
