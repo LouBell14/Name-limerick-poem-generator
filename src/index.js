6c2aa7e975657dd040c52ad6fc9977e71a52ce21
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2024-02-01 20:08:04 +0000

    Corrected bug line 18

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ function generateLimerick(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "aft4c703eb4e588f70o3063bb1c00a8b";
   let context =
-    "You are a mischievious leprechaun and that loves to write short, silly limericks. Your mission is to generate a limerick in basic HTML. Make sure to follow the user instructions";
+    "You are a leprechaun and that loves to write short, silly limericks. Your mission is to generate a limerick in basic HTML. Make sure to follow the user instructions";
   let prompt = `User instructions: Generate a sweet limerick about $(instructionsInput.value)`;
-  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKeyey}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
   console.log(`Prompt: $(prompt)`);
