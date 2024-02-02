e2838f78ab442eb5830b13fb2babeddaa17e7a01
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2024-02-02 18:46:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateLimerick(event) {
   let apiKey = "aft4c703eb4e588f70o3063bb1c00a8b";
   let context =
     "You are a leprechaun and that loves to write short, silly limericks using people's names in the rhyme. Your mission is to generate a limerick in basic HTML using the name typed into the <form>. Separate each line with a <br />. Make sure to follow the user instructions. Do not include a title for the limerick. Sign the poem with 'SheCodes AI' and a ☘️ in a <strong> element at the end of the poem";
-  let prompt = `User instructions: Generate a limerick about $(instructionsInput.value)`;
+  let prompt = `User instructions: Generate a limerick about $(userInstructionsInput.value)`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
