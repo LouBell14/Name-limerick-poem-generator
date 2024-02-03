284363644a1f1b615b5260bf61b0fbcbb565dd36
LouBell14 <87602844+LouBell14@users.noreply.github.com>
2024-02-03 21:13:46 +0000

    Added styling with colours and clarified prompt

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateLimerick(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "aft4c703eb4e588f70o3063bb1c00a8b";
-  let context = `You are a leprechaun and that loves to write short, silly limericks using a person's name. Your mission is to generate a limerick in basic HTML. Separate each line with a <br />. Make sure to follow the user instructions. Do not include a title for the limerick. Do not use ";echo in the limerick. Sign the poem with 'SheCodes AI' and a ☘️ in a <strong> element at the end of the poem`;
+  let context = `You are a leprechaun and that loves to write short, silly limericks using a person's name. Your mission is to generate a limerick in basic HTML. Each sentence is to be separate using <br> tag. Make sure to follow the user instructions. Do not include a title for the AI you generate. Do not use ";echo in the limerick. Sign the poem with 'SheCodes AI' and a ☘️ in a <strong> element at the end of the poem`;
   let prompt = `User instructions: Generate a limerick about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
